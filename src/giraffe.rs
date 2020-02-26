7f90618ed60e6c23d305d551743ae76f12b26fca
Sukyoung Jeong <suki.jeong@samsung.com>
2020-02-27 01:08:19 +0900

    minor fix

--- src/giraffe.rs
+++ src/giraffe.rs
@@ -5,36 +5,47 @@ pub fn data() -> clap::ArgMatches<'static> {
         .version("0.1")
         .author("Sukyoung Jeong <suki.jeong@samsung.com>")
         .about("is a graph tool")
-
-        .arg(Arg::with_name("datafile")
-             .help("Data csv file to draw graph")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("x_value")
-             .help("# of x value in the csv")
-             .short("x")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("y_value")
-             .help("# of x value in the csv")
-             .short("y")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("title")
-             .help("Title of the graph")
-             .long("title")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("x_label")
-             .help("X-axis label")
-             .long("x_label")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("y_label")
-             .help("Y-axis label")
-             .long("y_label")
-             .takes_value(true)
-             .required(true))
+        .arg(
+            Arg::with_name("datafile")
+                .help("Data csv file to draw graph")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("x_value")
+                .help("# of x value in the csv")
+                .short("x")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("y_value")
+                .help("# of x value in the csv")
+                .short("y")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("title")
+                .help("Title of the graph")
+                .long("title")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("x_label")
+                .help("X-axis label")
+                .long("x_label")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("y_label")
+                .help("Y-axis label")
+                .long("y_label")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
 
     return matches;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 extern crate csv;
 
-use gnuplot::{Figure, Color};
+use gnuplot::{Color, Figure};
 use std::env;
 use std::error::Error;
 use std::ffi::OsString;
 use std::process;
 
 use gnuplot::AxesCommon;
-
 
 mod giraffe;
 
@@ -18,14 +17,14 @@ fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
     }
 }
 
-fn eat(xval:usize, yval:usize) -> Result<(Vec<i64>, Vec<i64>), Box<dyn Error>> {
-	let mut x = Vec::new();
-	let mut y = Vec::new();
+fn eat(xval: usize, yval: usize) -> Result<(Vec<i64>, Vec<i64>), Box<dyn Error>> {
+    let mut x = Vec::new();
+    let mut y = Vec::new();
 
-	let file_path = get_first_arg()?;
-	let mut rdr = csv::ReaderBuilder::new()
-		.has_headers(false)
-		.from_path(file_path)?;
+    let file_path = get_first_arg()?;
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(file_path)?;
 
     for result in rdr.records() {
         let record = result?;
@@ -37,35 +36,37 @@ fn eat(xval:usize, yval:usize) -> Result<(Vec<i64>, Vec<i64>), Box<dyn Error>> {
 }
 
 fn main() {
-	let data = giraffe::data();
+    let data = giraffe::data();
 
-    let x_column = data.value_of("x_value").unwrap().parse::<usize>()
-    	.expect("x column should be an integer");
+    let x_column = data
+        .value_of("x_value")
+        .unwrap()
+        .parse::<usize>()
+        .expect("x column should be an integer");
 
-    let y_column = data.value_of("y_value").unwrap().parse::<usize>()
-    	.expect("y column should be an integer");
+    let y_column = data
+        .value_of("y_value")
+        .unwrap()
+        .parse::<usize>()
+        .expect("y column should be an integer");
 
     let title = data.value_of("title").unwrap();
     let x_label = data.value_of("x_label").unwrap();
     let y_label = data.value_of("y_label").unwrap();
 
-	match eat(x_column, y_column) {
-		Ok((x, y)) => {
-			let mut fg = Figure::new();
-			fg.set_title(title)
-				.axes2d()
-
-				.set_x_label(x_label, &[])
-				.set_y_label(y_label, &[])
-				.lines(&x, &y, &[Color("black")]);
-			fg.show().unwrap();
-
-		},
-		Err(err) => {
-			println!("{}", err);
-       		process::exit(1);
-		}
-
-	}
-    
+    match eat(x_column, y_column) {
+        Ok((x, y)) => {
+            let mut fg = Figure::new();
+            fg.set_title(title)
+                .axes2d()
+                .set_x_label(x_label, &[])
+                .set_y_label(y_label, &[])
+                .lines(&x, &y, &[Color("black")]);
+            fg.show().unwrap();
+        }
+        Err(err) => {
+            println!("{}", err);
+            process::exit(1);
+        }
+    }
 }
